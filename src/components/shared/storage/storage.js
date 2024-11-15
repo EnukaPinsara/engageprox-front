bd3a423c39621f8b9fd62c32cd9aebc184f8ae8c
dewminGH <trashancsgo@gmail.com>
2024-11-15 10:29:18 +0530

    state lib mount

---
+++ src/components/shared/storage/storage.js
@@ -0,0 +1,18 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+
+const useCounterStore = create(
+  persist(
+    set => ({
+      count: 0,
+      increaseCount: () => set(state => ({ count: state.count + 1 })),
+      decreaseCount: () => set(state => ({ count: state.count - 1 })),
+      resetCount: () => set({ count: 0 })
+    }),
+    {
+      name: 'counter-storage'
+    }
+  )
+);
+
+export default useCounterStore;
